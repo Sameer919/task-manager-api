b75037e93c9885f98fd224d2e32ac8496d23aada
Mohammed Sameer <79749863+Sameer919@users.noreply.github.com>
2026-05-05 15:19:21 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timezone
+from . import db
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    status = db.Column(db.String(20), default='pending') # pending or completed
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'status': self.status,
+            'created_at': self.created_at.isoformat() if self.created_at else None
+        }
